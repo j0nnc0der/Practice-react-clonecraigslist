444c30f553b5b17cb8832bac817e6ef93ebda97f
Jonn <jonnmaster1@gmail.com>
2019-05-04 23:55:26 +0100

    “updated”

--- assets/js/Craigslist/pages/Listings.js
+++ assets/js/Craigslist/pages/Listings.js
@@ -12,17 +12,18 @@ export default class Listings extends Component {
 		return (
 			<div className="listings-page">
 				<div className="container">
-					<section className="filter">
+					<section id="filter">
 						<div className="form-group price">
 							<label> Price </label>
-							<select name="min-price" className="max-price">
-								<option value="0">0</option>
-							</select>
-						</div>
-						<div className="min-price">
-							<select name="price" className="min-price">
-								<option value="1000"> 1000</option>
-							</select>
+							<div className="min-max">
+								<select name="min-price" className="min-price">
+									<option value="0">0</option>
+								</select>
+
+								<select name="max-price" className="max-price">
+									<option value="1000"> 1000</option>
+								</select>
+							</div>
 						</div>{' '}
 						<div className="form-group make">
 							<label> Make </label>
@@ -41,39 +42,44 @@ export default class Listings extends Component {
 							<div className="reset-btn">Reset</div>
 						</div>
 					</section>
-					{/* City: {match.params.city}
+				</div>
+
+				{/* City: {match.params.city}
 					Category: {match.params.category}
 					Listings: {match.params.listings} */}
 
-					<section className="list-view">
-						<section className="change-view">
-							<div className="form-group view-dropdown">
-								<select name="select-view" className="select-view">
-									<option value="gallery">Gallery view</option>
-									<option value="gallery">List view</option>
-									<option value="thumb">List view</option>
-								</select>
-							</div>
-							<div className="form-group sort-dropdown">
-								<select name="sort-dropdown" className="sort-dropdown">
-									<option value="gallery">Newest</option>
-								</select>
-							</div>
-						</section>{' '}
-						<section className="all-item">
-							<div className="item">
-								<div className="image">
-									<div className="price">$6100</div>
-									image
+				<section id="list-view">
+					<div className="container">
+						<div className="white-box">
+							<section className="change-view">
+								<div className="form-group view-dropdown">
+									<select name="select-view" className="select-view">
+										<option value="gallery">Gallery view</option>
+										<option value="list">List view</option>
+										<option value="thumb">List view</option>
+									</select>
 								</div>
-								<div className="details">
-									<h5>Title</h5>
-									<h6>City</h6>
+								<div className="form-group sort-dropdown">
+									<select name="sort-dropdown" className="sort-dropdown">
+										<option value="gallery">Newest</option>
+									</select>
 								</div>
-							</div>
-						</section>
-					</section>
-				</div>
+							</section>{' '}
+							<section className="all-items">
+								<div className="item">
+									<div className="image">
+										<div className="price">$6100</div>
+										image
+									</div>
+									<div className="details">
+										<h5>Title</h5>
+										<h6>City</h6>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</section>
 			</div>
 		);
 	}
